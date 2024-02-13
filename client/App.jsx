6c7ae1fd79be5b076d0739644f4c92a97c61428d
cyliang93 <cathyluong93@yahoo.com>
2024-02-12 18:38:10 -0800

    pushing branch to git

---
+++ client/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+    return(
+        <div>
+
+        </div>
+    )
+};
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(element);
